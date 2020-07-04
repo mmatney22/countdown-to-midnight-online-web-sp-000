8e3562c00d9ffc53e1c9c7c36bcb854e62605f90
Morgan Matney <morgan.matney89@gmail.com>
2020-07-04 14:34:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,6 +7,11 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(secs)
-  sleep(5)
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
