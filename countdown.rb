67598646bd9ddb8ef10b727a310f05f908f5c3bf
Morgan Matney <morgan.matney89@gmail.com>
2020-07-04 14:34:40 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,14 @@ def countdown(number)
     puts "#{number} SECOND(S)!"
     number -= 1
   end
-  return "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(secs)
-  sleep(5)
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
